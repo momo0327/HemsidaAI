0190f87b637d16c72929810564f004b8b40f2b69
mohamed Al-Safe <mohamedal-safe@MacBook-Air-som-tillhor-mohamed.local>
2024-09-27 17:55:58 +0200

    knzkNZ

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {}, // Tailwind CSS plugin
+    autoprefixer: {}, // Autoprefixer plugin
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}), // Conditionally adding cssnano
   },
 };
 
